9283df0bab0dea09afc587001b0f13b341b6005d
AttePerttola <57904504+AttePerttola@users.noreply.github.com>
2020-07-23 00:00:28 +0300

    lisätty while useita tiedostoja varten

--- my-grep.c
+++ my-grep.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,6 +12,7 @@ int main(int argc, const char* argv[]) {
     char *line;
     size_t len = 0;
     ssize_t nread;
+    int filecounter = 2;
 
     if (argc == 1) {
         printf("my-grep: searchterm [file...]\n");
@@ -33,24 +35,37 @@ int main(int argc, const char* argv[]) {
         }
         exit(0);
 
-    } else if (argc == 3) {
+    } else if (argc > 2) {
+        
         /*jos haettu termi on tyhjä ei löydetä mitää :D*/
         if (!strcmp(argv[1],"\0")) {
             printf("No matches found.\n");
             exit(1);
         }
-        fp = fopen(argv[2],"r");
-        if (fp == NULL) {
-            perror("my-grep: cannot open file.\n");
-            exit(1);
-        }
-        while((nread = getline(&line,&len,fp)) != -1) {
-            if (strstr(line,argv[1]) != NULL) {
-                printf("%s",line);
+        
+        while (filecounter <= argc-1) {
+            fp = fopen(argv[filecounter],"r");
+            if (fp == NULL) {
+                perror("my-grep: cannot open file.\n");
+                exit(1);
             }
+        
+            while((nread = getline(&line,&len,fp)) != -1) {
+                
+                if (strstr(line,argv[1]) != NULL) {
+                    
+                    //printf("%s",line);
+                    fwrite(line, nread, 1, stdout);
+                }
+                free(line);
+            }
+            
+            
+            fclose(fp);
+            filecounter++;
         }
-        free(line);
-        fclose(fp);
+        
+        
     } else {
         printf("Too many arguments.\n");
         exit(1);
