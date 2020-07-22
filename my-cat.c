02f2f2a4daa16a45380684f787a8ac849fbef229
AttePerttola <57904504+AttePerttola@users.noreply.github.com>
2020-07-22 20:04:15 +0300

    Update my-cat.c
    lisätty kommentteja

--- my-cat.c
+++ my-cat.c
@@ -7,17 +7,18 @@ int main(int argc, const char* argv[]) {
     int filecounter = 1;
     char buffer[1000];
 
-    if (argc == 1) {
+    if (argc == 1) { /*jos ei anneta tiedostoa, lopetetaan ajo*/
         printf("No file specified.\n");
         exit(0);
     }
-    if (argc > 1) {
-        while (filecounter <= argc-1) { /*käydään kaikki annetut argumentit läpi*/
+    if (argc > 1) { /*jos annetaan yksi tai enemmän tiedostoja käydään ne kaikki läpi*/
+        while (filecounter <= argc-1) { /* argc-1 koska argc ja argv indeksi lähtee eri numeroista*/
             fp = fopen(argv[filecounter],"r");
             if (fp == NULL) {
                 perror("my-cat: cannot open file.\n");
                 exit(1);
             }
+            /*käydään rivi kerrallaan tiedostoa läpi ja tulostetaan se terminaaliin*/
             while(fgets(buffer,1000,fp) != NULL) {
                 printf("%s",buffer);
             }
